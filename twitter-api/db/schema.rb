be5c1d5af5f25bd7c92e576e1dfcb514275e0ff8
Leonardo Teodoro <leonardo.teodoro@fabrica.locaweb.com.br>
2023-10-31 15:42:34 -0300

    feat: add user/auth rotes

--- twitter-api/db/schema.rb
+++ twitter-api/db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_24_200338) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_31_170517) do
   create_table "comments", force: :cascade do |t|
-    t.string "content", null: false
+    t.string "content"
     t.integer "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -24,6 +24,32 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_24_200338) do
     t.string "body", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider", default: "email", null: false
+    t.string "uid", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.boolean "allow_password_change", default: false
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.string "name"
+    t.string "nickname"
+    t.string "image"
+    t.string "email", default: "", null: false
+    t.boolean "admin"
+    t.text "tokens"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
   add_foreign_key "comments", "posts"
